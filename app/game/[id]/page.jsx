4a2d560fa5acded94ececf1a5abebabc814772ea
Plmeks <plmeks94@gmail.com>
2023-07-21 00:59:24 +0300

    Console errors fixed

--- app/game/[id]/page.jsx
+++ app/game/[id]/page.jsx
@@ -24,7 +24,7 @@ export default async function Page({ params }) {
           )}
         </div>
         <div className="p-4">
-          <p
+          <div
             className="text-justify"
             dangerouslySetInnerHTML={{ __html: game.description }}
           />
